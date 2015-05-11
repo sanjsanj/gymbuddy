ff9c79761f5f5c6a94220a0cfd4ec3c9fc2bbe7d
GJMcGowan <gemcgow@gmail.com>
2015-05-11 16:24:44 +0100

    Adding scheme?

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,15 @@ ActiveRecord::Schema.define(version: 20150511135843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "posts", force: :cascade do |t|
+    t.text     "activity"
+    t.text     "location"
+    t.time     "time"
+    t.date     "day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -42,12 +51,4 @@ ActiveRecord::Schema.define(version: 20150511135843) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "posts", force: :cascade do |t|
-    t.text     "activity"
-    t.text     "location"
-    t.time     "time"
-    t.date     "day"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
