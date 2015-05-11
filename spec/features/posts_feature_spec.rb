476a279ad763e9c6a9ca3d151fb0a3075616b8cf
GJMcGowan <gemcgow@gmail.com>
2015-05-11 17:18:52 +0100

    You have to be signed in to post

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 require 'spec_helpers/helpers'
 
 feature 'Posts' do
+  def sign_up
+    visit '/users/sign_up'
+    fill_in 'Email', with: 't@t.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    fill_in 'Name', with: 'George'
+    select('Male', :from => 'Gender')
+    fill_in 'Fitness bio', with: 'Powerlifting, also light golf'
+    select('Beginner', :from => 'Fitness level')
+    attach_file('user_image', 'spec/features/images/me.jpg')
+    click_button 'Sign up'
+  end
+
   context 'Website on initialization' do
     scenario 'should have no posts' do
       visit '/posts'
@@ -15,6 +28,7 @@ feature 'Posts' do
   end
   context 'When creating a post' do
     scenario 'it shows the post' do
+      sign_up
       visit '/posts'
       click_link 'New Post'
       select 'Swimming', from: 'Activity'
@@ -22,6 +36,11 @@ feature 'Posts' do
       select_date_and_time(DateTime.now)
       click_button 'Create Post'
       expect(page).to have_content 'Post was successfully created.'
+    end
+
+    scenario 'users who are not signed in cannot post' do
+      visit '/posts/new'
+      expect(page).to have_content 'You need to sign in'
     end
   end
 end
